e573e4cef0e82204c4e839dcd061db8af84883c6
JanOPedersen <jan.o.pedersen@mail.dk>
2021-08-26 08:50:56 +0200

    Update ViewController.py
    Consistency with Google Python Style Guide

--- ViewController.py
+++ ViewController.py
@@ -1,47 +1,47 @@
-from tkinter import *
+from tkinter import Listbox,Tk,Label,Entry,StringVar,Scrollbar,RIGHT,Y,Button,IntVar,Checkbutton
 import tkinter.messagebox as MessageBox
 import webbrowser
-from backend import *
+import backend
 
 # This is just a wrapper class for the static stuff in the backend.py file
 class ModelMySQL(object):
 
     def __init__(self):
-        self._connection = connect_to_db()
+        self._connection = backend.connect_to_db()
 
     def __del__(self):
-        disconnect_from_db(self._connection)
+        backend.disconnect_from_db(self._connection)
         
     @property
     def connection(self):
         return self._connection
 
     def insert(self, filename, filepath):
-        mySQLinsert(self._connection, filename, filepath)
+        backend.mySQLinsert(self._connection, filename, filepath)
 
     def delete(self, id):
-        mySQLdelete(self._connection, id)
+        backend.mySQLdelete(self._connection, id)
 
     def update(self, id, flags):
-        mySQLupdate(self._connection, id, flags)
+        backend.mySQLupdate(self._connection, id, flags)
 
     def getSelected(self, filename):
-        return mySQLgetSelected(self._connection, filename)
+        return backend.mySQLgetSelected(self._connection, filename)
 
     def getFromId(self, id):
-        return getFromId(self._connection, id)
+        return backend.getFromId(self._connection, id)
 
     def getFromKeywords(self, keywords, flags, retAll):
-        return getFromKeywords(self._connection, keywords, flags, retAll)
+        return backend.getFromKeywords(self._connection, keywords, flags, retAll)
 
     def getFromFilepath(self, filepath, flags, retAll):
-        return getFromFilepath(self._connection, filepath, flags, retAll)
+        return backend.getFromFilepath(self._connection, filepath, flags, retAll)
 
     def getFromFlags(self, flags):
-        return getFromFlags(self._connection, flags)
+        return backend.getFromFlags(self._connection, flags)
 
     def updateNumbers(self):
-        updateNumbers(self._connection)
+        backend.updateNumbers(self._connection)
 
 
 
@@ -61,43 +61,45 @@ class ViewController(object):
 
         controlsX = 10
         controlsY = 10
-        filepath = Label(self.root, text='Path', font = ('bold',10))
+        filepath = Label(self.root, text='Path', font = ('bold', 10))
         filepath.place(x=controlsX,y=controlsY)
 
         self.root.update()
-        filename = Label(self.root, text='Filename', font = ('bold',10))
+        filename = Label(self.root, text='Filename', font = ('bold', 10))
         filename.place(x=controlsX,y=filepath.winfo_height() + filepath.winfo_y())
 
         self.root.update()
-        id = Label(self.root, text='ID', font = ('bold',10))
+        id = Label(self.root, text='ID', font = ('bold', 10))
         id.place(x=controlsX,y=filename.winfo_height() + filename.winfo_y())
 
         self.root.update()
-        number = Label(self.root, text='number', font = ('bold',10))
+        number = Label(self.root, text='number', font = ('bold', 10))
         number.place(x=controlsX,y=id.winfo_height() + id.winfo_y())
 
         self.root.update()
         leftOffset = 5
         entriesX = 90
         self.e_filepath = Entry()
-        self.e_filepath.place(x=entriesX, y=filepath.winfo_y(), width=self.root.winfo_width() - entriesX - leftOffset)
+        self.e_filepath.place(x=entriesX, y=filepath.winfo_y(), 
+                              width=self.root.winfo_width() - entriesX - leftOffset)
 
         self.e_filename = Entry()
-        self.e_filename.place(x=entriesX, y=filename.winfo_y(), width=self.root.winfo_width() - entriesX - leftOffset)
+        self.e_filename.place(x=entriesX, y=filename.winfo_y(), 
+                              width=self.root.winfo_width() - entriesX - leftOffset)
 
         self.e_id = Entry()
         self.e_id.place(x=entriesX, y=id.winfo_y(), width=60)
 
         self.numberText = StringVar()
-        self.number_val = Label(self.root, font = ('bold',10),textvariable=self.numberText)
+        self.number_val = Label(self.root, font = ('bold',10), textvariable=self.numberText)
         self.number_val.place(x=entriesX, y=number.winfo_y(), width=60)
 
         # Add list box
         self.root.update()
-        listBoxHeight   = self.root.winfo_height() - self.e_filename.winfo_y() - self.e_filename.winfo_height() - 28 - 10
-        listBoxY        = self.e_id.winfo_y()
-        listBoxX        = 160
-        listBoxWidth    = width=self.root.winfo_width() - listBoxX - 25
+        listBoxHeight = self.root.winfo_height() - self.e_filename.winfo_y() - self.e_filename.winfo_height() - 28 - 10
+        listBoxY = self.e_id.winfo_y()
+        listBoxX = 160
+        listBoxWidth = width = self.root.winfo_width() - listBoxX - 25
         self.list = Listbox(self.root)
         self.list.place(x = listBoxX,y = listBoxY, width = listBoxWidth, height = listBoxHeight)
 
@@ -116,60 +118,63 @@ class ViewController(object):
         buttonsSpacing = 5
 
         ButtonTxts = ['insert', 'delete', 'update', 'get', 'get Selected', 'show Selected', 'clear all']
-        ButtonCommands = [self.insert, self.delete, self.update, self.get, self.getSelected, self.showSelected, self.clearAll]
+        ButtonCommands = [self.insert, self.delete, self.update, self.get, 
+                          self.getSelected, self.showSelected, self.clearAll]
 
         btnX = listBoxX
         for i in range(len(ButtonTxts)):
-            btn = Button(self.root, text=ButtonTxts[i],font=('italic',10),bg='white', command = ButtonCommands[i])
+            btn = Button(self.root, text=ButtonTxts[i], font=('italic', 10), 
+                         bg='white', command = ButtonCommands[i])
             btn.place(x = btnX + buttonsSpacing ,y = buttonsY)
             self.root.update()
             btnX += (buttonsSpacing + btn.winfo_width())
 
         # Add a label to show the number of results retrieved
-        nofHitsLabel = Label(self.root, text='# of results:', font = ('bold',10))
+        nofHitsLabel = Label(self.root, text='# of results:', font = ('bold', 10))
         nofHitsLabel.place(x = controlsX,y = buttonsY)
         self.root.update()
         self.nofHitsText = StringVar()
-        nofHits = Label(self.root, font = ('bold',10),textvariable=self.nofHitsText)
+        nofHits = Label(self.root, font = ('bold', 10), textvariable=self.nofHitsText)
         nofHits.place(x = controlsX + nofHitsLabel.winfo_width() + 5,y = buttonsY)
 
         # Add a checkbox at the end of the buttons that indicate if we want to retrieve everything or 
         # condition our retrieval on the checkboxes
         self.root.update()
         self.retAll = IntVar(value=1)
-        chkRetrieveAll = Checkbutton(self.root, text="retrieve all",variable=self.retAll, onvalue=1, offvalue=0)
+        chkRetrieveAll = Checkbutton(self.root, text="retrieve all", variable=self.retAll, onvalue=1, offvalue=0)
         chkRetrieveAll.place(x = btnX + buttonsSpacing,y = buttonsY)
 
         # Add check boxes for our PDF paper classifications
         self.flags = 0
-        classes = ['machine learning','deep learning','reinforcement learning','Google','Computer Vision','printed','speculative','mathematics']
+        classes = ['machine learning','deep learning','reinforcement learning',
+                   'Google','Computer Vision','printed','speculative','mathematics']
         self.vars = [IntVar() for x in [None] * len(classes)]
         CheckBoxesStartY = number.winfo_height() + number.winfo_y()
         CheckBoxesHeight = 20
         self.chkBoxes = []
         for i in range(len(classes)):
             self.root.update()
-            chkBox = Checkbutton(self.root, text=classes[i],variable=self.vars[i], onvalue=1, offvalue=0)
+            chkBox = Checkbutton(self.root, text=classes[i], variable=self.vars[i], onvalue=1, offvalue=0)
             chkBox.place(x = controlsX, y = CheckBoxesStartY + CheckBoxesHeight * i,height = CheckBoxesHeight)
             self.chkBoxes.append(chkBox)
 
         self.root.mainloop()
 
     def fillCheckBoxes(self,var):
+        def get_bit(num, i) -> int:
+            return (num & (1 << i)) != 0
+        
         for i in range(len(self.vars)):
-            self.vars[i].set(ViewController.get_bit(var, i))
+            self.vars[i].set(get_bit(var, i))
             if i == 5:
-                self.chkBoxes[i].configure(state = 'disabled' if ViewController.get_bit(var, i) == 1 else 'normal')
+                self.chkBoxes[i].configure(state = 'disabled' if get_bit(var, i) == 1 else 'normal')
                    
     # This is an unbound Python function which works like a static C++ class function
-    def getFlags(vars):
+    def getFlags(vars) -> int:
         flags = 0
         for i in range(len(vars)):
             flags += (vars[i].get() << i)
         return flags
-
-    def get_bit(num, i):
-        return (num & (1 << i)) != 0
 
     # TODO: we still have to implement the insert operation
     def insert(self):
@@ -186,7 +191,7 @@ class ViewController(object):
             self.list.delete(self.sel,self.sel)
             self.nofHitsText.set("")
             self.numberText.set("")
-        except Exception as e:
+        except ValueError as e:
             print(e)
             MessageBox.showinfo("DELETE STATUS","we got an exception")
 
@@ -194,18 +199,18 @@ class ViewController(object):
         try:
             self.flags = ViewController.getFlags(self.vars)
             self.model.update(self.e_id.get(), self.flags)
-        except Exception as e:
+        except ValueError as e:
             print(e)
             MessageBox.showinfo("UPDATE STATUS","we got an exception")
 
     # Given a database row we display it in the frontend
     def displayRow(self,row):
-        self.e_filename.delete(0,'end')
-        self.e_filepath.delete(0,'end')
-        self.e_id.delete(0,'end')
-        self.e_id.insert(0,row[0])
-        self.e_filename.insert(0,row[1])
-        self.e_filepath.insert(0,row[2])
+        self.e_filename.delete(0, 'end')
+        self.e_filepath.delete(0, 'end')
+        self.e_id.delete(0, 'end')
+        self.e_id.insert(0, row[0])
+        self.e_filename.insert(0, row[1])
+        self.e_filepath.insert(0, row[2])
         self.numberText.set(str(row[5]))
         self.flags = row[4]
         self.fillCheckBoxes(self.flags)
@@ -222,7 +227,8 @@ class ViewController(object):
             # In case we don't supply any seach information, but have the retrieve all checkbox
             # unchecked we use the flags as a query
             self.flags = ViewController.getFlags(self.vars)
-            if (self.e_id.get() == "" and self.e_filename.get() == "" and self.e_filepath.get() == "" and self.retAll.get() == 0):
+            if (self.e_id.get() == "" and self.e_filename.get() == "" 
+                and self.e_filepath.get() == "" and self.retAll.get() == 0):
                 rows = self.model.getFromFlags(self.flags)
                 self.nofHitsText.set(str(len(rows)))
 
@@ -233,20 +239,22 @@ class ViewController(object):
                 elif (len(rows) == 0):
                     MessageBox.showinfo("GET FROM FLAGS STATUS","no records found with given flags")
                 # In case of multiple matches fill the listbox so that the user can select
-                elif (len(rows) > 0):
+                elif rows:
                     self.displayRows(rows)
 
             # In case we have an ID entered we want to retrieve the corresponding database entry
             # and display the result
-            elif (self.e_id.get() != "" and self.e_filename.get() == "" and self.e_filepath.get() == ""):
+            elif (self.e_id.get() != "" and self.e_filename.get() == "" 
+                    and self.e_filepath.get() == ""):
                 rows = self.model.getFromId(self.e_id.get())
                 self.displayRow(rows[0])
-                self.list.delete(0,'end')
+                self.list.delete(0, 'end')
                 self.list.insert(1, rows[0][1])
 
             # in case we don't supply an ID but have an approximate file name, we want to find
             # the closest match in the database and update all fields (including the file name)
-            elif (self.e_filename.get() != "" and self.e_id.get() == "" and self.e_filepath.get() == ""):
+            elif (self.e_filename.get() != "" and self.e_id.get() == "" 
+                    and self.e_filepath.get() == ""):
                 # First we extract the keywords
                 filename = self.e_filename.get()
                 keywords = filename.split()
@@ -256,13 +264,14 @@ class ViewController(object):
                 # If there is only one result we fill the fields with it
                 if (len(rows) == 1):
                     self.displayRow(rows[0])
-                elif (len(rows) == 0):
+                elif not rows:
                     MessageBox.showinfo("GET STATUS","no records found with given keywords")
                 # In case of multiple matches fill the listbox so that the user can select
-                if (len(rows) > 0):
+                if rows:
                     self.displayRows(rows)
 
-            elif (self.e_filename.get() == "" and self.e_id.get() == "" and self.e_filepath.get() != ""):
+            elif (self.e_filename.get() == "" and self.e_id.get() == "" 
+                  and self.e_filepath.get() != ""):
                 # First we extract the windows sub path
                 filepath = self.e_filepath.get()
                 filepath = filepath. replace("\\", "/")
@@ -274,12 +283,12 @@ class ViewController(object):
                     self.displayRow(rows[0])
 
                 # In case of multiple matches fill the listbox so that the user can select
-                elif (len(rows) == 0):
+                elif not rows:
                     MessageBox.showinfo("GET STATUS","no records found with given filepath")
 
                 self.displayRows(rows)
                 
-        except Exception as e:
+        except ValueError as e:
             print(e)
             MessageBox.showinfo("GET STATUS","no records found with ID")
 
@@ -287,7 +296,7 @@ class ViewController(object):
         try:
             # If we where able to select something
             self.sel = self.list.curselection()
-            if (len(self.sel)) > 0:
+            if self.sel:
                 itm = self.list.get(self.sel)
                 rows = self.model.getSelected(itm)
 
@@ -295,7 +304,7 @@ class ViewController(object):
                 # records in the database, so just select the first one
                 self.displayRow(rows[0])
 
-        except Exception as e:
+        except ValueError as e:
                     print(e)
                     MessageBox.showinfo("GET STATUS","no records found with ID")
 
@@ -303,22 +312,22 @@ class ViewController(object):
         try:
             # If we where able to select something
             sel = self.list.curselection()
-            if (len(sel)) > 0:
+            if sel:
                 itm = self.list.get(sel)
                 rows = self.model.getSelected(itm)
                 fullpath = rows[0][2] + '/' + rows[0][1]
                 webbrowser.open(fullpath,new=2)
 
-        except Exception as e:
+        except ValueError as e:
             print(e)
             MessageBox.showinfo("SHOW SELECTED STATUS","no records found with ID")
                 
 
     def clearAll(self):
-        self.e_filename.delete(0,'end')
-        self.e_filepath.delete(0,'end')
-        self.e_id.delete(0,'end')
-        self.list.delete(0,self.list.size())
+        self.e_filename.delete(0, 'end')
+        self.e_filepath.delete(0, 'end')
+        self.e_id.delete(0, 'end')
+        self.list.delete(0, self.list.size())
         self.nofHitsText.set("")
         self.numberText.set("")
         for v in self.vars:
